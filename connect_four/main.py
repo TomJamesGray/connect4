272051d1dff871dc13b828111ec2f4de317f0925
TomJamesGray <tomg77@yahoo.co.uk>
2017-02-13 22:29:04 +0000

    Started basic implementation of menu

--- connect_four/main.py
+++ connect_four/main.py
@@ -1,9 +1,14 @@
 import kivy
 from kivy.app import App
+from kivy.core.window import Window
+from kivy.config import Config
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
 class ConnectFour(Widget):
+    pass
+
+class Menu(Widget):
     pass
 
 class ConnectFourApp(App):
@@ -11,4 +16,6 @@ class ConnectFourApp(App):
         return ConnectFour()
 
 def main():
+    Config.set("graphics","width","800")
+    Config.set("graphics","height","600")
     ConnectFourApp().run()
